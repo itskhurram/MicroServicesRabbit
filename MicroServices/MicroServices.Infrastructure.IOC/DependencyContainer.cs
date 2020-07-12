06786069ce1eb5185f7fb4b4f31588bc5aa65f41
Khurram <itsmeks@gmail.com>
2020-07-12 11:08:13 +0500

    Initial Commit of Infrastructure Dependency Container.

---
+++ MicroServices/MicroServices.Infrastructure.IOC/DependencyContainer.cs
@@ -0,0 +1,20 @@
+﻿using MicroServices.Domain.Core.Bus;
+using MicroServices.Infrastructure.Bus;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroServices.Infrastructure.IOC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
